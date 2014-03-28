c12efc0fcf1b8ba58a7fe697216f502c2935729a
caesarof007 <caesarof007@gmial.com>
2014-03-28 10:11:50 +0800

    增加main.c

---
+++ c_test/main.c
@@ -0,0 +1,14 @@
+/****************************************
+ *@filename main.c
+ *@brief
+ *@autor   lizongliang
+ *@date    2014.3.29
+ *@version 1.0
+ ***************************************/
+
+#include <stdio.h>
+
+int main(int argc,char *argv[])
+{
+    return 0;
+}
