0182b50445a3d6d8f0e9f62dcf78b91c4fd8929e
caesarof007 <caesarof007@gmial.com>
2014-03-29 12:34:21 +0800

    modify:add tuple.py

---
+++ python_test/datestruct/tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#filename tuple.py
+#run python tuple.py
+
+#tu is the same as list but a const
+
+tu = ("lee","boy","28")
+print "tu is ",tu
+print "tu[0] is ",tu[0]
+print "tu[2] is ",tu[2]
+print "tu[-1]is ",tu[-1]
+
+#print tu.insert(1."a") error
